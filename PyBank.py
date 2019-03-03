400d60237babb5a3c0f744102778a87e21566ce0
tolu1111 <35709864+tolu1111@users.noreply.github.com>
2019-03-02 21:05:34 -0600

    Add files via upload

---
+++ PyBank.py
@@ -0,0 +1,46 @@
+#Import Dependencies perform certain functions in python
+import os
+import csv
+
+# define where the data is located
+bankcsv = os.path.join("Resources", "budget_data.csv")
+
+# define empty lists for Date, profit/loss and profit and loss changes
+Profit_loss = []
+Date = []
+PL_Change = []
+
+# Read csv file
+with open(bankcsv, newline= '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csvheader = next(csvreader)
+
+# calculate the values for the profit/loss and the dates
+# store respective values in appropraite lists created earlier    
+    for row in csvreader:
+        Profit_loss.append(float(row[1]))
+        Date.append(row[0])
+
+#Print the calculated values
+print("Financial Analysis")
+print("-----------------------------------")
+print(f"Total Months: {len(Date)}")
+print(f"Total Revenue: {sum(Profit_loss)}")
+#Calculate the average revenue change, the greatest revenue increase and the greatest revenue decrease 
+for i in range(1,len(Profit_loss)):
+        PL_Change.append(Profit_loss[i] - Profit_loss[i-1])   
+        avg_PL_Change = (sum(PL_Change)/len(PL_Change))
+
+        max_PL_Change = max(PL_Change)
+
+        min_PL_Change = min(PL_Change)
+
+        max_PL_Change_date = str(Date[PL_Change.index(max(PL_Change)) + 1])
+        min_PL_Change_date = str(Date[PL_Change.index(min(PL_Change)) + 1])
+# print the calculated values
+print(f"Avereage Revenue Change: {round(avg_PL_Change,2)}")
+print(f"Greatest Increase in Revenue: {max_PL_Change_date}, {max_PL_Change}")
+print(f"Greatest Decrease in Revenue: {min_PL_Change_date}, {min_PL_Change}") 
+
+
---
+++ PyPoll.py
@@ -0,0 +1,78 @@
+#Import Dependencies perform certain functions in python
+import os
+import csv
+
+# define where the data is located
+electiondata = os.path.join("Resources", "election_data.csv")
+
+# define empty lists for the different contestants to store their recorded votes
+TotalVotes = []
+TotalKhan = []
+TotalCorrey = []
+TotalLi = []
+TotalOTooley = []
+
+# Read csv file 
+with open(electiondata, newline= '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csvheader = next(csvreader)
+# calculate the values for the different candidates
+# store respective values in appropraite lists created earlier  
+
+    for row in csvreader:
+        TotalVotes.append((row[2]))
+    
+        if (row[2]) == "Khan":
+            TotalKhan.append((row[2]))
+        elif (row[2]) == "Correy":
+            TotalCorrey.append((row[2]))
+        elif (row[2]) == "Li":
+            TotalLi.append((row[2]))
+        else:
+            TotalOTooley.append((row[2]))
+#calculate voting totals for each candidate
+KhanResults = int(len(TotalKhan))
+CorreyResults = int(len(TotalCorrey))
+LiResults = int(len(TotalLi))
+OTooleyResults =int(len(TotalOTooley))
+
+#calculate the percentage votes for each candidate with respect to total votes
+Khan_percent_votes = (int(len(TotalKhan))/int(len(TotalVotes)))
+Correy_percent_votes = (int(len(TotalCorrey))/int(len(TotalVotes)))
+Li_percent_votes = (int(len(TotalLi))/int(len(TotalVotes)))
+OTooley_percent_votes = (int(len(TotalOTooley))/int(len(TotalVotes)))
+
+
+#Define Election results
+ElectionResults = [KhanResults, CorreyResults, LiResults, OTooleyResults]
+
+#Calculate Election winner
+for winner in ElectionResults:  
+    if max(ElectionResults) == KhanResults:
+            ElectionWinner = "Khan"
+    elif max(ElectionResults) == CorreyResults:
+            ElectionWinner = "Correy"
+    elif max(ElectionResults) == LiResults:
+            ElectionWinner = "Li"
+    else: 
+            ElectionWinner = "O'Tooley"
+    
+
+
+# print the Election Results, total votes and percentages for each voter
+
+print("Election Results")
+print("-----------------------------------")
+print(f"Total Votes: {len(TotalVotes)}")
+print("-----------------------------------")
+print(f'Khan: {(format((Khan_percent_votes), ".3%"))} ({len(TotalKhan)})')
+print(f'Correy: {(format((Correy_percent_votes), ".3%"))} ({len(TotalCorrey)})')
+print(f'Li: {(format((Li_percent_votes), ".3%"))} ({len(TotalLi)})')
+print(f"O'Tooley: {(format((OTooley_percent_votes), '.3%'))} ({len(TotalOTooley)})")
+
+print("-----------------------------------")
+print(f"Winner: {ElectionWinner}")
+
+print("-----------------------------------")
+
